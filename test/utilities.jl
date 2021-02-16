07cdc581a434797c5962577e9e05b5a72137a7e4
Jarrett Revels <jarrettrevels@gmail.com>
2021-02-16 17:21:30 -0500

    test _iterator_for_column fast/slow paths

--- test/utilities.jl
+++ test/utilities.jl
@@ -1,49 +1,54 @@
 @testset "Onda.gather" begin
-     a = [(x=1, y="a", z="k"),
-          (x=2, y="b", z="j"),
-          (x=4, y="c", z="i"),
-          (x=4, y="d", z="h"),
-          (x=2, y="e", z="g"),
-          (x=5, y="f", z="f"),
-          (x=4, y="g", z="e"),
-          (x=3, y="h", z="d"),
-          (x=1, y="i", z="c"),
-          (x=5, y="j", z="b"),
-          (x=4, y="k", z="a")]
-     b = [(x=1, m=1),
-          (x=2, m=2),
-          (x=2, m=5),
-          (x=5, m=4),
-          (x=4, m=6)]
-     c = [(test="a", x=1, z=1.0),
-          (test="b", x=2, z=1.0),
-          (test="d", x=4, z=1.0),
-          (test="e", x=2, z=1.0),
-          (test="f", x=5, z=1.0),
-          (test="h", x=3, z=1.0),
-          (test="i", x=1, z=1.0),
-          (test="j", x=5, z=1.0),
-          (test="k", x=4, z=1.0)]
-     g = Onda.gather(:x, a, b, c; extract=(t, i) -> t[i])
-     dfg = Onda.gather(:x, DataFrame(a), DataFrame(b), DataFrame(c))
-     expected = Dict(1 => ([(x=1, y="a", z="k"), (x=1, y="i", z="c")],
-                           [(x=1, m=1)],
-                           [(test="a", x=1, z=1.0), (test="i", x=1, z=1.0)]),
-                     2 => ([(x=2, y="b", z="j"), (x=2, y="e", z="g")],
-                           [(x=2, m=2), (x=2, m=5)],
-                           [(test="b", x=2, z=1.0), (test="e", x=2, z=1.0)]),
-                     3 => ([(x=3, y="h", z="d")],
-                           NamedTuple{(:x, :m),Tuple{Int64,Int64}}[],
-                           [(test="h", x=3, z=1.0)]),
-                     4 => ([(x=4, y="c", z="i"), (x=4, y="d", z="h"), (x=4, y="g", z="e"), (x=4, y="k", z="a")],
-                           [(x=4, m=6)],
-                           [(test="d", x=4, z=1.0), (test="k", x=4, z=1.0)]),
-                     5 => ([(x=5, y="f", z="f"), (x=5, y="j", z="b")],
-                           [(x=5, m=4)],
-                           [(test="f", x=5, z=1.0), (test="j", x=5, z=1.0)]))
-     @test g == expected
-     @test keys(dfg) == keys(expected)
-     @test all(all(dfg[k] .== DataFrame.(expected[k])) for k in keys(dfg))
+      a = [(x=1, y="a", z="k"),
+           (x=2, y="b", z="j"),
+           (x=4, y="c", z="i"),
+           (x=4, y="d", z="h"),
+           (x=2, y="e", z="g"),
+           (x=5, y="f", z="f"),
+           (x=4, y="g", z="e"),
+           (x=3, y="h", z="d"),
+           (x=1, y="i", z="c"),
+           (x=5, y="j", z="b"),
+           (x=4, y="k", z="a")]
+      b = [(x=1, m=1),
+           (x=2, m=2),
+           (x=2, m=5),
+           (x=5, m=4),
+           (x=4, m=6)]
+      c = [(test="a", x=1, z=1.0),
+           (test="b", x=2, z=1.0),
+           (test="d", x=4, z=1.0),
+           (test="e", x=2, z=1.0),
+           (test="f", x=5, z=1.0),
+           (test="h", x=3, z=1.0),
+           (test="i", x=1, z=1.0),
+           (test="j", x=5, z=1.0),
+           (test="k", x=4, z=1.0)]
+      dfa, dfb, dfc = DataFrame(a), DataFrame(b), DataFrame(c)
+      g = Onda.gather(:x, a, b, c; extract=(t, i) -> t[i])
+      dfg = Onda.gather(:x, dfa, dfb, dfc)
+      expected = Dict(1 => ([(x=1, y="a", z="k"), (x=1, y="i", z="c")],
+                            [(x=1, m=1)],
+                            [(test="a", x=1, z=1.0), (test="i", x=1, z=1.0)]),
+                      2 => ([(x=2, y="b", z="j"), (x=2, y="e", z="g")],
+                            [(x=2, m=2), (x=2, m=5)],
+                            [(test="b", x=2, z=1.0), (test="e", x=2, z=1.0)]),
+                      3 => ([(x=3, y="h", z="d")],
+                            NamedTuple{(:x, :m),Tuple{Int64,Int64}}[],
+                            [(test="h", x=3, z=1.0)]),
+                      4 => ([(x=4, y="c", z="i"), (x=4, y="d", z="h"), (x=4, y="g", z="e"), (x=4, y="k", z="a")],
+                            [(x=4, m=6)],
+                            [(test="d", x=4, z=1.0), (test="k", x=4, z=1.0)]),
+                      5 => ([(x=5, y="f", z="f"), (x=5, y="j", z="b")],
+                            [(x=5, m=4)],
+                            [(test="f", x=5, z=1.0), (test="j", x=5, z=1.0)]))
+      @test g == expected
+      @test keys(dfg) == keys(expected)
+      @test all(all(dfg[k] .== DataFrame.(expected[k])) for k in keys(dfg))
+
+      # test both the fast path + fallback path for Onda._iterator_for_column
+      @test Onda._iterator_for_column(dfa, :x) === dfa.x
+      @test Onda._iterator_for_column(a, :x) == dfa.x
 end
 
 @testset "Onda.validate_on_construction" begin
