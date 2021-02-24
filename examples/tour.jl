cefe751783b44c9c1fa7c229fe3932d5c10de957
Jarrett Revels <jarrettrevels@gmail.com>
2021-02-24 06:46:57 -0500

    Update examples/tour.jl

--- examples/tour.jl
+++ examples/tour.jl
@@ -134,7 +134,7 @@ transform(view(signals, findall(==(target), signals.recording), :),
           AsTable(:) => ByRow(load) => :samples)
 
 # `mmap` sample data for a given LPCM signal:
-target = first(s for s in eachrow(signals) if s.file_format=="lpcm")
+target = first(s for s in eachrow(signals) if s.file_format == "lpcm")
 samples = Onda.mmap(target)
 
 # Delete all sample data for a given recording (uncomment the
