08250a6c8fc32e0094aad0ceadf9e03cb5abeaa7
Jarrett Revels <jarrettrevels@gmail.com>
2021-02-25 07:19:31 -0500

    add basic mmap convenience function (#63)

--- src/samples.jl
+++ src/samples.jl
@@ -424,6 +424,30 @@ function load(file_path, file_format::AbstractLPCMFormat, info::SamplesInfo, spa
 end
 
 """
+    Onda.mmap(signal)
+
+Return `Onda.mmap(signal.file_path, SamplesInfo(signal))`, throwing an `ArgumentError` if `signal.file_format != "lpcm"`.
+"""
+function mmap(signal)
+    signal.file_format == "lpcm" || throw(ArgumentError("unsupported file_format for mmap: $(signal.file_format)"))
+    return mmap(signal.file_path, SamplesInfo(signal))
+end
+
+"""
+    Onda.mmap(mmappable, info::SamplesInfo)
+
+Return `Samples(data, info, true)` where `data` is created via `Mmap.mmap(mmappable, ...)`.
+
+`mmappable` is assumed to reference memory that is formatted according to the Onda Format's canonical
+interleaved LPCM representation in accordance with `info.sample_type` and `channel_count(info)`. No
+explicit checks are performed to ensure that this is true.
+"""
+function mmap(mmappable, info::SamplesInfo)
+    data = reshape(Mmap.mmap(mmappable, Vector{info.sample_type}), (channel_count(info), :))
+    return Samples(data, info, true)
+end
+
+"""
     store(file_path, file_format::Union{AbstractString,AbstractLPCMFormat}, samples::Samples)
 
 Serialize the given `samples` to `file_format` and write the output to `file_path`.
