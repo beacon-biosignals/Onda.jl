5b0d663a643f01e608c1bcbbac71e1e4f5f15a2a
Jarrett Revels <jarrettrevels@gmail.com>
2021-03-25 08:52:15 -0400

    oops

--- test/signals.jl
+++ test/signals.jl
@@ -108,7 +108,7 @@ end
     seekstart(io)
     for roundtripped in (read_signals(signals_file_path; materialize=false, validate_schema=false),
                          read_signals(signals_file_path; materialize=true, validate_schema=true),
-                         Onda.materialize(read_signals(io; validate_schema=true))
+                         Onda.materialize(read_signals(io)),
                          read_signals(io; validate_schema=true))
         roundtripped = collect(Tables.rows(roundtripped))
         @test length(roundtripped) == length(signals)
