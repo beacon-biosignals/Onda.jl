50f44dcc389d0fa76a77da25c32b3ac031c9db2d
Jarrett Revels <jarrettrevels@gmail.com>
2021-03-28 14:38:14 -0400

    bug fix

--- test/signals.jl
+++ test/signals.jl
@@ -109,7 +109,7 @@ end
     for roundtripped in (read_signals(signals_file_path; materialize=false, validate_schema=false),
                          read_signals(signals_file_path; materialize=true, validate_schema=true),
                          Onda.materialize(read_signals(io)),
-                         read_signals(io; validate_schema=true))
+                         read_signals(seekstart(io); validate_schema=true))
         roundtripped = collect(Tables.rows(roundtripped))
         @test length(roundtripped) == length(signals)
         for (r, s) in zip(roundtripped, signals)
