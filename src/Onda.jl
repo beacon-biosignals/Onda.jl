7d7b88ebc1861cfe2b7ba66eea328ab0625152d0
Jarrett Revels <jarrettrevels@gmail.com>
2021-07-27 18:25:55 -0400

    add maxlog=1 to manual deprecations + explanatory comment

--- src/Onda.jl
+++ src/Onda.jl
@@ -29,6 +29,12 @@ export Samples, encode, encode!, decode, decode!, load, store
 #####
 ##### upgrades/deprecations
 #####
+# For some functions below, we handroll deprecations via `@warn`.
+#
+# In these cases, we don't use `@deprecate` because the method we're "redirecting to" for the deprecation path
+# (e.g. `_deprecated_read_table`) is different than the method that we're suggesting downstream callers use
+# instead (`Legolas.read`). `Base.depwarn` could possibly be utilized here, but isn't an officially documented
+# function (though it's probably fine to use in practice). Thus, we settle for using `@warn ... maxlog=1`.
 
 function _deprecated_read_table(io_or_path, schema=nothing)
     table = Legolas.read_arrow(io_or_path)
@@ -37,13 +43,13 @@ function _deprecated_read_table(io_or_path, schema=nothing)
 end
 
 function read_signals(io_or_path; validate_schema::Bool=true)
-    @warn "`Onda.read_signals(io_or_path)` is deprecated, use `Legolas.read(io_or_path)` instead"
+    @warn "`Onda.read_signals(io_or_path)` is deprecated, use `Legolas.read(io_or_path)` instead" maxlog=1
     return _deprecated_read_table(io_or_path, validate_schema ? Legolas.Schema("onda.signal@1") : nothing)
 end
 export read_signals
 
 function read_annotations(io_or_path; validate_schema::Bool=true)
-    @warn "`Onda.read_annotations(io_or_path)` is deprecated, use `Legolas.read(io_or_path)` instead"
+    @warn "`Onda.read_annotations(io_or_path)` is deprecated, use `Legolas.read(io_or_path)` instead" maxlog=1
     return _deprecated_read_table(io_or_path, validate_schema ? Legolas.Schema("onda.annotation@1") : nothing)
 end
 export read_annotations
@@ -66,7 +72,7 @@ export read_annotations
                        sample_type, sample_rate, custom...))
 
 function validate(::SamplesInfo)
-    @warn "validate(::SamplesInfo) is deprecated; avoid invoking this method in favor of calling `validate(::Samples)`"
+    @warn "validate(::SamplesInfo) is deprecated; avoid invoking this method in favor of calling `validate(::Samples)`" maxlog=1
     return nothing
 end
 
